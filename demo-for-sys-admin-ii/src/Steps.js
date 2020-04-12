92a30180617490cb735a60a1819a840aff54626e
nhaberland <nhaberland97@gmail.com>
2020-04-12 17:57:24 -0400

    Updated Steps

--- demo-for-sys-admin-ii/src/Steps.js
+++ demo-for-sys-admin-ii/src/Steps.js
@@ -9,19 +9,110 @@ function Steps(){
             <h1>
                 Steps to Reproduce This Project
             </h1>
-            <p>
-            Install Mongo.db on your computer
-my-file-server
-Install Node JS
-Install Yarn package manager for node js globally
-Install react-script and nodemon globally
-"yarn global add react-scripts"
-"yarn global add nodemon"
-In frontend and backend directory run the command "yarn start"
-Command to start backend "nodemon app.js"
-Command to start front-end is "yarn start"
-
-            </p>
+            <table>
+                <tbody>
+                    <tr>
+                    Use git to clone the project on the frontend and backend server git clone https://github.com/CriusNyx/SysAdminII
+                    </tr>
+                    <tr>
+                        <h2>Installation:</h2>
+                    </tr>
+                    <tr>
+                        <h3>Frontend:</h3>
+                    </tr>
+                    <tr>
+                        <p>
+                        Install the following dependancies using your systems package manager: 
+                        </p>
+                    </tr>
+                    <tr>
+                                    
+                        docker
+                    </tr>
+                    <tr>
+                        node.js
+                    </tr>
+                    <tr>
+                        yarn (look at instructions on yarn package managers website)
+                    </tr>
+                    <tr>
+                        <h3>Backend:</h3>
+                    </tr>
+                    <tr>
+                        Install the following dependencies using your systems package manager:
+                    </tr>
+                    <tr>
+                        node.js
+                    </tr>
+                    <tr>
+                        yarn (look at instructions on yarn package managers website)
+                    </tr>
+                    <tr>
+                        <h2>Configuration:</h2>
+                    </tr>
+                    <tr>
+                        <h3>Frontend:</h3>
+                    </tr>
+                    <tr>
+                        <p>
+                            Open port 80 and change the serverurl variable in the FileManager.js and SignIn.js files to match the url for your backend server.
+                        </p>
+                    </tr>
+                    <tr>
+                        <h3>Backend:</h3>
+                    </tr>
+                    <tr>
+                        <p>
+                            Open port 80.
+                        </p>
+                    </tr>
+                    <tr>
+                        <h2>Deployment:</h2>
+                    </tr>
+                    <tr>
+                        <h3>Frontend:</h3>
+                    </tr>
+                    <tr>
+                        Run the following command in the SysAdminII directory:
+                    </tr>
+                    <tr>
+                        sudo bash build-and-run.sh
+                    </tr>
+                    <tr>
+                        The scrupt will build and deploy the server.
+                    </tr>
+                    <tr>
+                        <h3>Backend:</h3>
+                    </tr>
+                    <tr>
+                        Run the following command in the SysAdminII/backend directory:
+                    </tr>
+                    <tr>
+                        sudo back start-server.sh
+                    </tr>
+                    <tr>
+                        The script will build and deploy the backend server.
+                    </tr>
+                    <tr>
+                        <h2>Updating:</h2>
+                    </tr>
+                    <tr>
+                        <h3>Frontend:</h3>
+                    </tr>
+                    <tr>
+                        Followthe deployment instructions. The build-and-run.sh script will update the server automatically.
+                    </tr>
+                    <tr>
+                        <h3>Backend:</h3>
+                    </tr>
+                    <tr>
+                        Run the following command in the SysAdminII directory:
+                    </tr>
+                    <tr>
+                        git pull https://github.com/CriusNyx/SysAdminII
+                    </tr>
+                </tbody>
+            </table>
         </header>
     </div>
     )
