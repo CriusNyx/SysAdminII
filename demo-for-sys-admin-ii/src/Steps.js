470e0b11eda975b7e4f7dad1691fffa47a280ce3
nhaberland <nhaberland97@gmail.com>
2020-04-12 14:38:56 -0400

    Modified Steps

--- demo-for-sys-admin-ii/src/Steps.js
+++ demo-for-sys-admin-ii/src/Steps.js
@@ -8,7 +8,16 @@ function Steps(){
                 Steps to Reproduce This Project
             </h1>
             <p>
-            
+            Install Mongo.db on your computer
+my-file-server
+Install Node JS
+Install Yarn package manager for node js globally
+Install react-script and nodemon globally
+"yarn global add react-scripts"
+"yarn global add nodemon"
+In frontend and backend directory run the command "yarn start"
+Command to start backend "nodemon app.js"
+Command to start front-end is "yarn start"
 
             </p>
         </header>
